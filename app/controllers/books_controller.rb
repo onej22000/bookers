0c2c8e93513d8a955be047f7aa8b31cde3a5b716
EC2 Default User <ec2-user@ip-172-31-0-179.ec2.internal>
2022-01-11 13:18:50 +0000

    投稿をindexに並べるようにした

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,28 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+ private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
+end
