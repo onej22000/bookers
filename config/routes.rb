b28342c73a2fed9c45904c1cb7e67e11a6885569
EC2 Default User <ec2-user@ip-172-31-0-179.ec2.internal>
2022-01-13 15:39:55 +0000

    css修正開始

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'top' => "homes#top"
-  get 'new' => "books#new"
-  get 'index' => "books#index"
-  get 'show' => "books#show"
-  get 'edit' => "books#edit"
+  resources :books
   get '/' => "homes#top"
-  get 'books' => "books#index"
-  post 'books' => 'books#create'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
